e4a3719e49f81ae45f57f6438036c33e22dfa8cb
Marco Fernandes <marcofernandes75@gmail.com>
2023-10-13 14:41:19 +0100

    Feat: Add advanced custom block

--- web/modules/custom/workshop_blocks_example/blocks/index.js
+++ web/modules/custom/workshop_blocks_example/blocks/index.js
@@ -1,4 +1,12 @@
-import { registerBlockType } from "@wordpress/blocks";
+import { registerBlockStyle, registerBlockType } from "@wordpress/blocks";
 import { myBlock } from "./my-block";
+import PersonCard from "./person-card";
 
 registerBlockType("workshop-blocks-example/my-block", myBlock);
+registerBlockType("workshop-blocks-example/person-card", PersonCard);
+
+// Usually defined at theme level
+// registerBlockStyle('workshop-blocks-example/person-card', {
+//   name: 'image-right',
+//   label: 'Image right'
+// });
---
+++ web/modules/custom/workshop_blocks_example/blocks/person-card/save.js
@@ -0,0 +1,24 @@
+import { useBlockProps, InnerBlocks } from "@wordpress/block-editor";
+
+const Save = ({ attributes }) => {
+  const { image_url, image_uuid, name, position } = attributes;
+
+  return (
+    <div {...useBlockProps.save()}>
+      <img
+        data-entity-type="file"
+        data-entity-uuid={image_uuid}
+        src={image_url}
+      />
+      <div>
+        <h2>{name}</h2>
+        <p>{position}</p>
+        <div>
+          <InnerBlocks.Content />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Save;
