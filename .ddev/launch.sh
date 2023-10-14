db00c8b0ebb598fa09a97a8e8e8739d5d2080259
Marco Fernandes <marcofernandes75@gmail.com>
2023-10-14 12:16:38 +0100

    Fix: Tweaks and fixes; Update DB and files;

--- .ddev/launch.sh
+++ .ddev/launch.sh
@@ -5,9 +5,10 @@ echo "Logging in and launching..."
 
 site_uri=$(ddev exec "drush st --format=list --fields uri")
 uri_length=${#site_uri}
+((uri_length++))
 login_url=$(ddev exec "drush uli /")
 login_path=$(echo "${login_url}" | cut -c ${uri_length}-)
-
+echo ${login_path}
 ddev launch "${login_path}"
 
 echo "Done."
